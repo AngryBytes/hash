767848a81027150faa10bb5427abfea470b99afc
Maurice Fonk <git@naneau.net>
2013-09-09 12:08:50 -0400

    cleaning up HMAC
    only using strings for HMAC generation, as that's what it's defined for.

--- src/AngryBytes/Hash/HMAC.php
+++ src/AngryBytes/Hash/HMAC.php
@@ -7,10 +7,16 @@
  * @copyright       Copyright (c) 2010 Angry Bytes BV (http://www.angrybytes.com)
  */
 
+use Angrybytes\Hash\Hash;
+
+use \InvalidArgumentException;
+
 /**
  * HMAC
  *
  * HMAC creator
+ *
+ * This class will generate hashes to be used as HMAC
  *
  * @category        AngryBytes
  * @package         Hash
@@ -18,16 +24,16 @@
 class HMAC
 {
     /**
-     * Method to use
+     * Algorithm to use
      *
      * @var string
      **/
-    private $method;
+    private $algorithm;
 
     /**
      * Constructor
      *
-     * @param string $algorithm
+     * @param  string $algorithm
      * @return void
      **/
     public function __construct($algorithm)
@@ -36,24 +42,41 @@ class HMAC
     }
 
     /**
-     * Get the method to use
+     * Does this platform support an algorithm?
      *
-     * @return string
-     */
-    public function getMethod()
+     * @return bool
+     **/
+    public static function platformSupports($algorithm)
     {
-        return $this->method;
+        return in_array($algorithm, hash_algos());
     }
 
     /**
-     * Set the method to use
+     * Get the algorithm to use
      *
-     * @param string $method
+     * @return string
+     */
+    public function getAlgorithm()
+    {
+        return $this->algorithm;
+    }
+
+    /**
+     * Set the algorithm to use
+     *
+     * @param  string $algorithm
      * @return HMAC
      */
-    public function setMethod($method)
+    public function setAlgorithm($algorithm)
     {
-        $this->method = $method;
+        // Sanity check
+        if (!self::platformSupports($algorithm)) {
+            throw new InvalidArgumentException(sprintf(
+                '"%s" is not a supported hash algorithm on this platform'
+            ));
+        }
+
+        $this->algorithm = $algorithm;
 
         return $this;
     }
@@ -61,20 +84,60 @@ class HMAC
     /**
      * Create an HMAC
      *
-     * @param string $sharedSecret
-     * @param mixed $what1
-     * @param mixed $what2
+     * This method accepts multiple variables as input, but is restricted to
+     * strings. All input will be concatenated before hashing.
+     *
+     * @param  string $sharedSecret
+     * @param  mixed  $what1
+     * @param  mixed  $what2
      * ...
-     * @param mixed $whatN
+     * @param  mixed  $whatN
      * @return string
      **/
     public function hmac($sharedSecret)
     {
         // Get data as a string of passed args
         $args = func_get_args();
-        array_shift($args);
-        $data = serialize($args);
 
-        return hash_hmac('md5', $data, $sharedSecret);
+        // Shift the shared secret off
+        array_shift($args);
+
+        // Get the data concatenated
+        $data = '';
+        foreach ($args as $index => $arg) {
+
+            // Sanity check
+            if (!is_string($arg)) {
+                throw new InvalidArgumentException(sprintf(
+                    'Received a non-string argument at "%s"',
+                    $index
+                ));
+            }
+
+            $data .= $arg;
+        }
+
+        return hash_hmac(
+            $this->getAlgorithm(),
+            $data,
+            $sharedSecret
+        );
+    }
+
+    /**
+     * Check if a (received) message has a valid HMAC
+     *
+     * @param  string $message
+     * @param  string $hmac
+     * @param  string $sharedSecret
+     * @return bool
+     **/
+    public function validHmac($message, $hmac, $sharedSecret)
+    {
+        // The HMAC as it should be for our shared secret
+        $shouldHave = $this->hmac($sharedSecret, $message);
+
+        // Compare
+        return Hash::compare($hmac, $shouldHave);
     }
 }
